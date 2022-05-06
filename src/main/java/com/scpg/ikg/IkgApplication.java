064950800e668b25c135a3c04d54dd4fb76b8c81
c7cek <mustafaciceknote8@gmail.com>
2022-05-06 23:42:11 +0300

    app.prop

--- src/main/java/com/scpg/ikg/IkgApplication.java
+++ src/main/java/com/scpg/ikg/IkgApplication.java
@@ -1,35 +1,41 @@
 package com.scpg.ikg;
 
+import com.scpg.ikg.business.abstracts.IRoleService;
+import com.scpg.ikg.business.abstracts.IUserService;
+import com.scpg.ikg.core.entities.Role;
+import com.scpg.ikg.core.entities.User;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.util.ArrayList;
+
 @SpringBootApplication
 public class IkgApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(IkgApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(IkgApplication.class, args);
+    }
 
-	@Bean
-	PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
-//	    @Bean
-//		CommandLineRunner run(IUserService userService, IRoleService roleService) {
-//        return args -> {
-//
-//            userService.add(new User(0, "isoo", "test@test1.com", "12345", true, new ArrayList<>()));
-//            userService.add(new User(0, "kutsa1", "gürlek@gürlek.com", "12345", true, new ArrayList<>()));
-//
-//            roleService.add(new Role(0, "admin"));
-//            roleService.add(new Role(0, "user"));
-//
-//            userService.addRoleToUser("isoo", "admin");
-//            userService.addRoleToUser("kutsa1", "admin");
-//        };
-//    }
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    CommandLineRunner run(IUserService userService, IRoleService roleService) {
+        return args -> {
+
+            userService.add(new User(0, "c7cek", "mustafaciceknote8@gmail.com", "12345", true, new ArrayList<>()));
+            Role admin = new Role();
+            admin.setId(0);
+            admin.setName("admin");
+            roleService.add(admin);
+            userService.addRoleToUser("c7cek", "admin");
+        };
+    }
 
 }
